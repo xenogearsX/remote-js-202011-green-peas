cff12c67459bf38da2e1a86d5602d4401e73e114
Gijix <orlando.srnic@gmail.com>
2021-01-26 12:06:51 +0100

    ScreenJeux adde by olivier/orlando

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react'
+import ScreenJeux from './screen/ScreenJeux'
 
 class App extends React.Component {
   render() {
     return (
     <div className="App">
+      <ScreenJeux />
     </div>
     );
   }
---
+++ src/screen/ScreenJeux.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ScreenJeux.css';
+import from './components/'
+import from './components/'
+import from './components/'
+import from './components/'
+
+function ScreenJeux(){
+    return(
+
+<div className="ScreenJeux">
+    < /> 
+    < /> 
+    < /> 
+    < /> 
+    </div>
+    )
+}
+export default ScreenJeux
