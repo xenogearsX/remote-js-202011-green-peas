b239b787c4c8085587ba860178517ad89997d9a1
NicoBellick <nicolas.soumier@gmail.com>
2021-01-27 13:59:49 +0100

    Introduction v01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import React from 'react'
+import BounceIntro from './components/Zoom.js';
 
 class App extends React.Component {
   render() {
     return (
     <div className="App">
+      <BounceIntro />
+      
+      
     </div>
     );
   }
---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+
+const array =[{
+    image:""
+    quote: ""
+    className: top-left
+    compteur: 0
+}
+]
+class Cards extends React.Component {
+    render(){
+        return(
+            <img></img>
+            <quote></quote>
+            <button>En savoir plus</button>
+            <button>-></button>
+        )
+    }
+}
