a9d21507062e270637a782f31cc3eb7ea7cf3409
NicoBellick <nicolas.soumier@gmail.com>
2021-01-27 14:20:44 +0100

    Add Intro in dev

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react'
 import Home from './screen/Home'
 import Nav from './components/Nav';
 import Header from './components/Header';
+import BounceIntro from './components/BounceIntro';
 
 class App extends React.Component {
   render() {
     return (
     <div>
+      <BounceIntro />
       <Nav />
       <Header />
       <Home />
