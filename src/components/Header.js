16516581f6805a83d4c79813a230f1115d6faf5f
xenogearsX <xenogears@hotmail.fr>
2021-01-27 11:00:24 +0100

    move swipeMenu to class

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,22 +4,13 @@ import Identity from "./Identity";
 import BurgerMenu from "./BurgerMenu";
 
 class Header extends React.Component {
-    render() {
-    return (
-      <header id="header" onLoad={swipeMenu}>
-        <Identity />
-        <BurgerMenu />
-      </header>
-    );
-  }
-}
-
-const swipeMenu = (event) => {
+  swipeMenu = () => {
     const mainMenu = document.querySelector("#menu");
     const burgerMenu = document.querySelector("#menu-burger");
-    document.addEventListener("click", function(event) {
-      if(mainMenu.className === ''){ return;}
-        else if (event.target.closest("#menu") || event.target.closest("#menu-burger")) return;
+    document.addEventListener("click", function (event) {
+      if (mainMenu.className === "") {
+        return;
+      } else if (event.target.closest("#menu") || event.target.closest("#menu-burger")) return;
       mainMenu.classList.add("invisible");
       burgerMenu.removeAttribute("class", "clicked");
     });
@@ -41,6 +32,15 @@ const swipeMenu = (event) => {
       },
       false
     );
+  };
+  render() {
+    return (
+      <header id="header" onLoad={this.swipeMenu}>
+        <Identity />
+        <BurgerMenu />
+      </header>
+    );
   }
+}
 
 export default Header;
