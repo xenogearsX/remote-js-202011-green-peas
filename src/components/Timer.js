5f63871b2a3fcbe7663de2e2ed2ad930c3f8e03f
Gijix <orlando.srnic@gmail.com>
2021-01-28 16:32:18 +0100

    essai timmer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,18 +2,41 @@ import React from 'react'
 import bouton from './Bouton'
 import './timer.css'
 
+class Timer extends React.Component {
 
-const setTimer= () =>{
-    
-// let sec = 10
+let counter = 30
+let intervalId = null
+let timer = document.getElementById("bip")
+let button = document.getElementById("button")
+function start(){
+  button.disabled = true
+  intervalId = setInterval(bip, 1000)
+  }
 
-// let actualisation = setTimeout("compte_a_rebours();", 1000);
-}
+function bip() {
+    counter--
+    if(counter == 0) finish()
+    else {	
+        timer.innerHTML = counter + " secondes restantes"
+    }
+  }	
+    function finish() {
+      clearInterval(intervalId)
+      timer.innerHTML = "TERMINE!"
+      button.disabled = false
+      counter = 30	
+  }
 
-const Timer = () => {
+    render(){
     return(
-    <button onClick={setTimer()}>START TIMER</button>
-        );
+        <div>
+            <button class="button" onclick="start()" id="button">Start Game</button>
+            <div id="bip" class="compteur"></div>
+        </div>
+        )
+    }
 }
+    
+
 
 export default Timer
