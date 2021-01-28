d25e7046bec630f0ac9bc4639dada56b1321b231
Caroline Morlet <carolinemorlet@gmail.com>
2021-01-28 16:01:46 +0100

    create footer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import React from 'react'
+import React from 'react';
+import Footer from './components/Footer';
+import './components/Footer.css';
 
 class App extends React.Component {
   render() {
     return (
-    <div className="App">
-    </div>
+      <div className="App">
+        <Footer />
+      </div>
     );
   }
 }
