95c4072f477c82d12d206663cbaa4c0444baed88
xenogearsX <xenogears@hotmail.fr>
2021-01-30 11:45:33 +0100

    create card element to render search result

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,15 @@
+import React from "react";
 
-const array =[{
-    image:""
-    quote: ""
-    className: top-left
-    compteur: 0
-}
-]
 class Cards extends React.Component {
-    render(){
-        return(
-            <img></img>
-            <quote></quote>
-            <button>En savoir plus</button>
-            <button>-></button>
-        )
-    }
+  render() {
+    return (
+      <div className="card">
+        <p>{prop.value}</p>
+        <p>
+          Empreinte environnementale :{" "}
+          {prop.results[0]["Score_unique_EF_(mPt/kg_de_produit)"]}
+        </p>
+      </div>
+    );
+  }
 }
