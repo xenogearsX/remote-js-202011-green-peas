39a783fdf5d0a92ddf2c0427959c9e32fbf0f21b
oliviermod8 <oliviermod8@gmail.com>
2021-02-03 16:59:16 +0100

    Change the earth image + first modif on responsive design

--- src/components/GameAccess.js
+++ src/components/GameAccess.js
@@ -8,12 +8,20 @@ class GameAccess extends React.Component {
     render (){
         return (
             <div className="BodyGame">
-                <div className="Rules"> 
-                    <div id="titleRules"> REGLES DU JEUX </div>
-                    <img src={timer_earth} alt="timer-earth" id="imageEarth"/>
-                    <p id="rulesText"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, numquam molestias? Error tempora expedita assumenda, aut optio inventore, voluptatem sapiente ullam consequatur repellat quia placeat voluptatum doloribus quisquam ipsa repudiandae! </p>
-                    <Timer />
-                 </div>
+                <div className="introRules">
+                    <div className="Rules"> 
+                         <div id="titleRules"> REGLES DU JEUX </div>
+                         <p id="rulesTextIntro"> Tu as une faim de 🐺 ? Alors commence le jeu et créé ton menu Entrée + Plat + Dessert + Boisson  😋 </p>
+                    </div>
+                    <div id="imageDiv"><img src={timer_earth} alt="timer-earth" id="imageEarth"/></div>
+                </div>       
+                <div className="Bar"></div>
+                <div className="AstuceParag">                   
+                    <div className="Astuce">💚 Astuces </div>
+                    <p id="rulesText">⚠️ #1 Tu dois prendre tes décisions rapidement car la 🌍 est déjà en danger 
+                    <br></br> ⚠️ #2 Pense que chacun de tes choix à un impact 🔥 sur la planète </p>
+                </div>
+                <div className="ButtonStart"><Timer /></div> 
             </div>
         )
     }
