c9d972f043c5c24d8aff7f281ac5a11504d0a404
oliviermod8 <oliviermod8@gmail.com>
2021-02-03 12:55:39 +0100

    import Timer component in GameAccess.js

--- src/components/GameAccess.js
+++ src/components/GameAccess.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Timer from './Timer';
 import './GameAccess.css';
 import timer_earth from '../data/images/timer_earth.png';
+
 
 class GameAccess extends React.Component {
     render (){
@@ -10,9 +12,12 @@ class GameAccess extends React.Component {
                     <div id="titleRules"> REGLES DU JEUX </div>
                     <img src={timer_earth} alt="timer-earth" id="imageEarth"/>
                     <p id="rulesText"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, numquam molestias? Error tempora expedita assumenda, aut optio inventore, voluptatem sapiente ullam consequatur repellat quia placeat voluptatum doloribus quisquam ipsa repudiandae! </p>
+                    <Timer />
                  </div>
             </div>
         )
     }
 }
+
+
 export default GameAccess; 
