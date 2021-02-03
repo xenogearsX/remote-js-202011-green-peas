2717de36e70fca129dd2049fbbb867c813bf54da
oliviermod8 <oliviermod8@gmail.com>
2021-02-03 19:17:07 +0100

    Responsive max 1024px done

--- src/components/GameAccess.js
+++ src/components/GameAccess.js
@@ -10,16 +10,17 @@ class GameAccess extends React.Component {
             <div className="BodyGame">
                 <div className="introRules">
                     <div className="Rules"> 
-                         <div id="titleRules"> REGLES DU JEUX </div>
-                         <p id="rulesTextIntro"> Tu as une faim de 🐺 ? Alors commence le jeu et créé ton menu Entrée + Plat + Dessert + Boisson  😋 </p>
+                         <div id="titleRules"> REGLES DU JEU </div>
+                         <p id="rulesTextIntro"> Tu as une faim de 🐺 ? Alors commence le jeu pour créer ton menu Entrée + Plat + Dessert + Boisson  😋 </p>
                     </div>
                     <div id="imageDiv"><img src={timer_earth} alt="timer-earth" id="imageEarth"/></div>
                 </div>       
                 <div className="Bar"></div>
                 <div className="AstuceParag">                   
                     <div className="Astuce">💚 Astuces </div>
-                    <p id="rulesText">⚠️ #1 Tu dois prendre tes décisions rapidement car la 🌍 est déjà en danger 
-                    <br></br> ⚠️ #2 Pense que chacun de tes choix à un impact 🔥 sur la planète </p>
+                    <p id="rulesText">#1 Tu dois prendre tes décisions rapidement car la 🌍 est déjà en danger ⚠️
+                    <br></br> #2 Pense que chacun de tes choix à un impact 🔥 sur la planète 
+                    <br></br> #3 N'oublie pas, les 🍅 ne poussent pas l'hiver ⛄ </p>
                 </div>
                 <div className="ButtonStart"><Timer /></div> 
             </div>
