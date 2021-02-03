c463e6a62f502be4edeca2a4affd76a386c09016
oliviermod8 <oliviermod8@gmail.com>
2021-02-03 12:27:09 +0100

    add DIV title + rules text + css

--- src/components/GameAccess.js
+++ src/components/GameAccess.js
@@ -7,8 +7,9 @@ class GameAccess extends React.Component {
         return (
             <div className="BodyGame">
                 <div className="Rules"> 
+                    <div id="titleRules"> REGLES DU JEUX </div>
                     <img src={timer_earth} alt="timer-earth" id="imageEarth"/>
-       
+                    <p id="rulesText"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, numquam molestias? Error tempora expedita assumenda, aut optio inventore, voluptatem sapiente ullam consequatur repellat quia placeat voluptatum doloribus quisquam ipsa repudiandae! </p>
                  </div>
             </div>
         )
