6e4e534c86ba42e51531da9cb9872bdb9f7c31dc
xenogearsX <xenogears@hotmail.fr>
2021-02-10 12:21:11 +0100

    create bilan component for screenbilan

---
+++ src/components/Bilan.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Bilan extends React.Component {
+
+    render(){
+        return(
+            <div className="">
+            </div>
+        )
+    }
+}
+export default Bilan
