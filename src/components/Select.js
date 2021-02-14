9a4e644628954fdf0870034578e53fa941d41888
xenogearsX <xenogears@hotmail.fr>
2021-02-14 18:46:31 +0100

    cleaning + create randomTen

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import test from "../data/json/csvjson-light.json"
+import test from "../data/json/test.json"
 import defaultImgDish from "../data/images/dish.svg"
 
 export default class Select extends Component {
 state = {
   imgDefault : defaultImgDish,
-  imgDefaultAlt : 'Choisi ton entréé',
+  imgDefaultAlt : 'Choisis ton entrée',
   classBt : 'bouton off'
 }
-
-  render() {
+render() {
+const gameFood = test.filter(data => data['Catégorie Jeux'].includes(this.props.name))
+const remainingGameFood = gameFood.slice(0);
+const randomTen = []
+while (randomTen.length<10) randomTen.push(remainingGameFood.splice(Math.floor(Math.random()*remainingGameFood.length),1)[0]);
     return (
       <>
       <select>
-        {test.filter(data => data['Catégorie Jeux'].includes(this.props.name)).map((item) => (
+        {randomTen.map((item) => (
           <option value={item['Score unique EF (mPt/kg de produit)']}>{item["Nom du Produit en Français"]}</option>
         ))}
       </select>
