af071eb8741a4c1612b4d514445f3c2ff07ce2af
xenogearsX <xenogears@hotmail.fr>
2021-02-27 09:19:24 +0100

    move intervalId to state + clearinterval on unmount

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 
-let intervalId = null;
-
 class Timer extends React.Component {
   state = {
     counter: null,
+    intervalId: null,
   };
   timer = () => {
-    this.setState({ counter: 20 });
-    intervalId = setInterval(this.bip, 1000);
+    this.setState({ counter: 20, intervalId: setInterval(this.bip, 1000) });
   };
   bip = () => {
     this.setState({ counter: this.state.counter - 1 });
     if (this.state.counter === 0) {
-      clearInterval(intervalId);
+      clearInterval(this.state.intervalId);
     }
   };
   componentDidMount() {
     this.timer();
   }
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
   render() {
     return (
       <div>
         <div id="bip" className="compteur">
-          {this.state.counter === 0 ? "TERMINE !" : this.state.counter + " s."}
+          {this.state.counter + " s."}
         </div>
         {this.state.counter === 0 ? <Redirect to="/gameover" /> : null}
       </div>
